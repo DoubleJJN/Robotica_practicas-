c764a6e0cd2a6dd785cded6e7f4691dbedc65236
Lanraxx <xxxenekoxxx@gmail.com>
2025-10-04 11:26:04 +0200

    straight_odometry fixed

--- L_ortiz_J_ni/controllers/straight_odometry/MyRobot.cpp
+++ L_ortiz_J_ni/controllers/straight_odometry/MyRobot.cpp
@@ -24,7 +24,7 @@ MyRobot::MyRobot() : Robot()
     _x = _y = _theta = 0.0; // robot pose variables 
     _sr = _sl = 0.0; // displacement right and left wheels
 
-    _x_goal = 8, _y_goal =0, 
+    _x_goal = 8, _y_goal = 0;
     _theta_goal = atan2((_y_goal - _y),(_x_goal- _x)); // target pose 
 
     // Motor Position Sensor initialization
@@ -114,23 +114,35 @@ void MyRobot::run()
 //////////////////////////////////////////////
 void MyRobot::compute_odometry()
 {
-  float sr_meters;
-  float sl_meters;
-  float delta_sr;
-  float delta_sl;
+  // 1. Calcular el desplazamiento lineal (sr y sl almacenan la posición total, no el delta)
+  float current_sr = WHEEL_RADIUS * _right_wheel_sensor->getValue();
+  float current_sl = WHEEL_RADIUS * _left_wheel_sensor->getValue();
   
-  sr_meters = WHEEL_RADIUS * _right_wheel_sensor->getValue();
-  sl_meters = WHEEL_RADIUS * _left_wheel_sensor->getValue();
+  float delta_sr = current_sr - _sr; // Delta_sr
+  float delta_sl = current_sl - _sl; // Delta_sl
   
-  delta_sr = sr_meters - _sr;
-  delta_sl = sl_meters - _sl;
+  // 2. Calcular los componentes de la ecuación
+  float delta_s_media = (delta_sr + delta_sl) / 2.0;
+  float delta_theta = (delta_sr - delta_sl) / WHEELS_DISTANCE; // Calculo de Delta_theta
+
+  // 3. Aplicar las actualizaciones de odometría
+  // Ángulo usado para el cálculo de X e Y: theta_intermedio = theta_actual + Delta_theta/2
+  float theta_intermedio = _theta + (delta_theta / 2.0); 
+
+  _x = _x + (delta_s_media * cos(theta_intermedio));
+  _y = _y + (delta_s_media * sin(theta_intermedio));
   
-  _x = _x + ((delta_sr+delta_sl)/2) * cos(_theta + (delta_sr - delta_sl)/2*WHEELS_DISTANCE);
-  _y = _y + ((delta_sr+delta_sl)/2) * sin(_theta + (delta_sr - delta_sl)/2*WHEELS_DISTANCE);
-  _theta = _theta + ((delta_sr+delta_sl)/WHEELS_DISTANCE);
+  // 4. Actualizar la orientación final
+  _theta = _theta + delta_theta;
   
-  
-}        
+  // 5. Normalizar theta (CRUCIAL para navegación a largo plazo)
+  // M_PI debe estar disponible con #include <math.h> (ya incluido en MyRobot.h)
+  _theta = fmod(_theta + M_PI, 2.0 * M_PI) - M_PI; 
+
+  // 6. Actualizar las posiciones acumuladas para el siguiente paso
+  _sr = current_sr;
+  _sl = current_sl;
+}      
 //////////////////////////////////////////////
 
 double MyRobot::convert_bearing_to_degrees()
@@ -153,10 +165,14 @@ void MyRobot::print_odometry()
 
 bool MyRobot::goal_reached()
 {
-  // function to be completed by students
-  // ...
+  float x_target = 8.0; 
+  float tolerance = 0.1; // Se detiene cuando x está entre 7.9 y 8.1 metros
+  
+  // Condición de parada: Si la posición actual en X es mayor o igual al objetivo
+  if (_x >= (x_target - tolerance)) {
+      return true;
+  }
   
   return false;
-
 }
 //////////////////////////////////////////////
